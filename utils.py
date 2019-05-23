a0d3631b977cc487dfbff4385fbb03757e1b1a7b
Fernando Barscevicius <fbarscevicius@marketdata.com.br>
2019-05-23 09:25:29 -0300

    modelo finalizado

--- utils.py
+++ utils.py
@@ -3,9 +3,11 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
+from sklearn.metrics import mean_squared_error
 from typing import Union
 
-_FLOAT_OR_STR = Union[float, str]
+_FLOAT_OR_STR    = Union[float, str]
+_SERIES_OR_ARRAY = Union[pd.core.series.Series, np.ndarray]
 
 
 def convert_alcohol_str(x: _FLOAT_OR_STR) -> float: 
@@ -77,3 +79,51 @@ def univariate_plot(ncol: int, nrow: int, data: pd.DataFrame, target: list) -> N
     plt.tight_layout();
     plt.show();
     plt.gcf().clear();
+    
+    
+def get_coefs(colnames: list, coefs: np.ndarray, sort: bool = True) -> pd.DataFrame:
+    """
+    Retorna um DataFrame com duas colunas: (nome da variável, valor do coeficiente)
+    
+    Args:
+        colnames (list): nomes das variáveis
+        coefs (array): coeficientes retornados pelo modelo
+        sort (bool): ordenar o retorno de acordo com abs(coefs)
+
+    Returns:
+        DataFrame
+    """
+    
+    out = list(zip(colnames, coefs))
+    
+    if sort:
+        out.sort(key=lambda x: abs(x[1]), reverse=True)
+        
+    return pd.DataFrame(out, columns=['variavel', 'valor'])
+
+
+def rmse(y_true: _SERIES_OR_ARRAY, y_pred: _SERIES_OR_ARRAY) -> float:
+    """
+    Calcula a Raíz do Erro Quadrático Médio
+    
+    Args:
+        y_true (pd.Series or np.ndarray): vetor de respostas observadas
+        y_pred (pd.Series or np.ndarray): vetor de respostas preditas
+        
+    Returns:
+        valor do RMSE
+    """
+    
+    return mean_squared_error(y_true, y_pred)**0.5
+
+
+def plot_coef_hbar(data: pd.DataFrame) -> None:
+    """
+    Plota barras horizontais dos coeficientes do modelo
+    
+    Args:
+        data (pd.DataFrame): DataFrame retornado pela função `get_coefs`
+    """
+    plt.figure(figsize=(10, 8))
+    plt.barh(data.variavel, data.valor)
+    plt.title('Variável x Coeficiente')
